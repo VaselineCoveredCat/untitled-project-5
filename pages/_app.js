3bed939716b86a48b239c39882c86dd1c047ee78
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-08 19:54:57 +0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
+}
