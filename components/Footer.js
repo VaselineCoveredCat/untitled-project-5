7543f4b53788d028c33d99e6c297d9ce71f91a1c
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-08 19:54:47 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-teal-6 gap-6 grid grid-1 mt-6 text-teal-1 p-6">
+      <p className="text-center">&copy; 2022 Vaseline Covered Cat</p>
+    </footer>
+  );
+}
