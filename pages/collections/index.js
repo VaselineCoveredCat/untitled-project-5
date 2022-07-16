9a6896c98de86adaf44742b843a2f751ef593634
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-16 10:42:44 +0700

    Update index.js

--- pages/collections/index.js
+++ pages/collections/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import Link from "next/link";
 import { createClient } from "@supabase/supabase-js";
+import Catalogue from "../../components/Catalogue";
 
 export async function getStaticProps() {
   const supabase = createClient(
@@ -30,43 +30,7 @@ export default function Collections(props) {
           <h1>The Collections</h1>
           {props.items ? (
             <React.Fragment>
-              <ul className="gap-6 sm-gap-2 grid grid-col-2 md-grid-col-4 mt-6">
-                {props.items.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <Link href="/">
-                        <a className="overflow-hidden text-ellipsis w-100 whitespace-nowrap">
-                          <div className="overflow-hidden">
-                            <picture>
-                              <source
-                                srcSet={`/images/${item.name}.avif`}
-                                type="image/avif"
-                              />
-                              <source
-                                srcSet={`/images/${item.name}.webp`}
-                                type="image/webp"
-                              />
-                              <img
-                                alt=""
-                                aria-hidden="true"
-                                className="aspect-9-16"
-                                encoding="async"
-                                src={`/images/${item.name}.jpg`}
-                              />
-                            </picture>
-                          </div>
-                          <span className="capitalize">
-                            {item.name.replaceAll("-", " ")}
-                          </span>
-                          <br />
-                          <span className="sr-only">Price</span>
-                          <b>Rp {item.price}</b>
-                        </a>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
+              <Catalogue items={props.items} />
             </React.Fragment>
           ) : (
             <p className="text-center">Error occured</p>
