d6d3f1e5639b7b7ed91f904fd970b568128314df
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-18 21:17:45 +0700

    Update [slug].js

--- pages/collections/[slug].js
+++ pages/collections/[slug].js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { createClient } from "@supabase/supabase-js";
+import styles from "../../styles/slug.module.css";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -32,7 +33,7 @@ export default function Product(props) {
       </Head>
 
       <main>
-        <article className="gap-6 sm-gap-2 grid grid-col-1 sm-grid-col-2 mx-6">
+        <article className={styles.article}>
           <picture>
             <source
               srcSet={`/images/${props.items[0].slug}.avif`}
@@ -45,18 +46,88 @@ export default function Product(props) {
             <img
               alt=""
               aria-hidden="true"
-              className="aspect-9-16"
               encoding="async"
               src={`/images/${props.items[0].slug}.jpg`}
             />
           </picture>
 
-          <div>
-            <h1 className="text-h3">{props.items[0].name}</h1>
+          <div className={styles.div}>
+            <h1>{props.items[0].name}</h1>
+
             <p>
               <span className="sr-only">Price</span>
               {`Rp ${props.items[0].price}`}
             </p>
+
+            <p>{props.items[0].desc}</p>
+
+            <form className={styles.form}>
+              <p>Size</p>
+
+              <div>
+                <div>
+                  <input
+                    aria-label="small size"
+                    className={styles.one}
+                    defaultChecked
+                    id="small"
+                    name="sizes"
+                    type="radio"
+                    value="small"
+                  />
+                  <label className="sr-only" htmlFor="small">
+                    S
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    aria-label="medium size"
+                    className={styles.two}
+                    id="medium"
+                    name="sizes"
+                    type="radio"
+                    value="medium"
+                  />
+                  <label className="sr-only" htmlFor="medium">
+                    M
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    aria-label="large size"
+                    className={styles.three}
+                    id="large"
+                    name="sizes"
+                    type="radio"
+                    value="large"
+                  />
+                  <label className="sr-only" htmlFor="large">
+                    L
+                  </label>
+                </div>
+              </div>
+            </form>
+
+            <details>
+              <summary>Shipping</summary>
+              <p>
+                Please note estimated shipping times include processing time at
+                the warehouse and are calculated from the date the order is
+                placed.
+              </p>
+            </details>
+
+            <details>
+              <summary>Returns</summary>
+              <p>
+                All orders (subject to our returns policy) can be returned up to
+                28 days after the date of purchase. Please note, we cannot offer
+                refunds on beauty, pierced jewellery or on swimwear if the
+                hygiene seal is not in place or has been broken.
+              </p>
+            </details>
           </div>
         </article>
       </main>
