873ebe7772d3f0425b297e2684cd660a65d8ebdd
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-19 09:24:58 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+import styles from "../styles/footer.module.css";
+
 export default function Footer() {
   return (
-    <footer className="bg-teal-6 gap-6 grid grid-1 mt-6 text-teal-1 p-6">
-      <p className="text-center">&copy; 2022 Vaseline Covered Cat</p>
+    <footer className={styles.footer}>
+      <p>&copy; 2022 Vaseline Covered Cat</p>
     </footer>
   );
 }
