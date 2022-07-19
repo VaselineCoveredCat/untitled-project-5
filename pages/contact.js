327a7c27b2fddfa5dbba338be4d54e0e0db47a34
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-19 11:55:00 +0700

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,31 +1,41 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/contact.module.css";
+
 export default function Contact() {
   return (
-    <main>
-      <article className="mx-6">
-        <form className="gap-6 sm-gap-2 grid grid-col-1 mx-auto sm-max-w-50">
-          <div>
-            <label htmlFor="name">Name</label>
-            <input id="name" name="name" type="text" />
-          </div>
+    <React.Fragment>
+      <Head>
+        <title>Contact Us - Untitled Project 5</title>
+      </Head>
 
-          <div>
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" />
-          </div>
+      <main>
+        <article className={styles.article}>
+          <form>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input id="name" name="name" type="text" />
+            </div>
 
-          <div>
-            <label htmlFor="subject">Subject</label>
-            <input id="subject" name="subject" type="text" />
-          </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input id="email" name="email" type="email" />
+            </div>
 
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message"></textarea>
-          </div>
+            <div>
+              <label htmlFor="subject">Subject</label>
+              <input id="subject" name="subject" type="text" />
+            </div>
 
-          <button className="bg-teal-6 p-button text-teal-1">Send</button>
-        </form>
-      </article>
-    </main>
+            <div>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message"></textarea>
+            </div>
+
+            <button>Send</button>
+          </form>
+        </article>
+      </main>
+    </React.Fragment>
   );
 }
