be7ae35c390607e9a114a9fc049f0cf6a5267c18
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-20 12:26:20 +0700

    Update index.js

--- pages/collections/index.js
+++ pages/collections/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 import { createClient } from "@supabase/supabase-js";
 import Catalogue from "../../components/Catalogue";
 import styles from "../../styles/collections.module.css";
@@ -10,13 +11,15 @@ export async function getStaticProps() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
-  let res = await supabase
+  const res = await supabase
     .from("dress")
-    .select("*")
+    .select("*", { count: "exact" })
     .order("name", { ascending: true })
     .limit(20);
 
-  return { props: { items: res.data } };
+  const totalPages = Math.ceil(res.count / 20);
+
+  return { props: { items: res.data, pages: totalPages } };
 }
 
 export default function Collections(props) {
@@ -33,6 +36,26 @@ export default function Collections(props) {
           {props.items ? (
             <React.Fragment>
               <Catalogue items={props.items} />
+
+              <section>
+                <nav aria-label="pagination">
+                  <ul className={styles.page}>
+                    <li>
+                      <Link href="/collections">
+                        <a aria-disabled="true">Prev</a>
+                      </Link>
+                    </li>
+
+                    <li>{`Page 1 of ${props.pages}`}</li>
+
+                    <li>
+                      <Link href="/collections/page/2">
+                        <a>Next</a>
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </section>
             </React.Fragment>
           ) : (
             <p className={styles.error}>Error occured</p>
