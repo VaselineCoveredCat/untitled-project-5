662df753a74a373ddf28cd3661f58664e1d46c78
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-25 11:32:00 +0700

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -10,56 +10,94 @@ export default function Nav() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <button
-          aria-controls="menu"
-          aria-expanded={isOpen}
-          aria-haspopup="true"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <img alt="" aria-hidden="true" focusable="false" src="/menu.svg" />
-          <span className="sr-only">Menu</span>
-        </button>
+        <div>
+          <button
+            aria-controls="menu"
+            aria-expanded={isOpen}
+            aria-haspopup="true"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <img
+              alt=""
+              aria-hidden="true"
+              focusable="false"
+              src="/menu.svg"
+              title="Menu"
+            />
+            <span className="sr-only">Menu</span>
+          </button>
 
-        <ul
-          className={isOpen ? styles.mobile : styles.desktop}
-          id="menu"
-          role="list"
-        >
-          <li>
-            <Link href="/">
-              <a
-                aria-current={router.asPath === "/" ? "page" : undefined}
-                onClick={() => setIsOpen(false)}
-              >
-                Home
-              </a>
-            </Link>
-          </li>
+          <ul
+            className={isOpen ? styles.mobile : styles.desktop}
+            id="menu"
+            role="list"
+          >
+            <li>
+              <Link href="/">
+                <a
+                  aria-current={router.asPath === "/" ? "page" : undefined}
+                  onClick={() => setIsOpen(false)}
+                >
+                  Home
+                </a>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/collections">
-              <a
-                aria-current={
-                  router.asPath === "/collections" ? "page" : undefined
-                }
-                onClick={() => setIsOpen(false)}
-              >
-                Collections
-              </a>
-            </Link>
-          </li>
+            <li>
+              <Link href="/collections">
+                <a
+                  aria-current={
+                    router.asPath === "/collections" ? "page" : undefined
+                  }
+                  onClick={() => setIsOpen(false)}
+                >
+                  Collections
+                </a>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/contact">
-              <a
-                aria-current={router.asPath === "/contact" ? "page" : undefined}
-                onClick={() => setIsOpen(false)}
-              >
-                Contact
-              </a>
-            </Link>
-          </li>
-        </ul>
+            <li>
+              <Link href="/contact">
+                <a
+                  aria-current={
+                    router.asPath === "/contact" ? "page" : undefined
+                  }
+                  onClick={() => setIsOpen(false)}
+                >
+                  Contact
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <Link href="/account">
+            <a aria-current={router.asPath === "/account" ? "page" : undefined}>
+              <img
+                alt=""
+                aria-hidden="true"
+                focusable="false"
+                src="/user.svg"
+                title="Login"
+              />
+              <span className="sr-only">Login</span>
+            </a>
+          </Link>
+
+          <Link href="/cart">
+            <a aria-current={router.asPath === "/cart" ? "page" : undefined}>
+              <img
+                alt=""
+                aria-hidden="true"
+                focusable="false"
+                src="/cart.svg"
+                title="Cart"
+              />
+              <span className="sr-only">Cart</span>
+            </a>
+          </Link>
+        </div>
       </nav>
     </header>
   );
